d69c751314da21d84ec38b7fe6a52da6a9f74d75
Hyunmin Kim <human.gim@gmail.com>
2020-12-10 15:43:10 -0500

    Update plot.r

--- src/plot.r
+++ src/plot.r
@@ -15,3 +15,24 @@ myboxplot = function(df,features,group){
         ggplot(aes(x=feature, y=value,fill=feature)) +       
         geom_boxplot() + facet_wrap(~group)
 }
+
+#
+# Heatmap of markers
+#  markers: (feautre, type) columns
+
+myheatmap=function(seuratobj, markers){
+	require(ComplexHeatmap)
+	require(Matrix.utils)
+	require(tibble)
+	require(dplyr)
+	avg = t(seuratobj$RNA@data) %>%    
+		aggregate.Matrix( groupings=seuratobj@meta.data$seurat_clusters, fun = "mean") %>%              
+		t() %>% data.frame(check.names=F) %>% rownames_to_column(var="feature")
+	avg1=merge(avg,markers,by="feature")
+	avg.m=avg1[,2:(ncol(avg))]
+	row.names(avg.m) = make.names(avg1$feature,unique=TRUE)
+	ha=rowAnnotation( type=avg1$type )
+	Heatmap((avg.m), row_split=avg1$type,row_title_rot=0,
+		row_names_gp = gpar(fontsize = 8)) # right_annotation=ha)
+}
+
