6f4722f34be01f9ecb94ce1100aff4e2e7cfe016
Hyunmin Kim <human.gim@gmail.com>
2020-12-17 16:50:37 -0500

    Add files via upload

---
+++ src/merge-cellranger.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+usage="
+	$BASH_SOURCE <input> <output>
+
+	input: merged seurat object 
+	output: seurat object <output>.rds
+"
+if [ $# -lt 1 ];then echo "$usage"; return; fi
+cat<<'EOF'| sed s#OUTPUT#$2# | sed s#INPUT#$1#  > $2.rcmd
+input="INPUT";
+output="OUTPUT"; 
+require(Seurat)
+require(dplyr)
+inp=read.table(input,header=T);
+d.list=list();
+j=1; for( i in 1:nrow(inp)){ 
+	f=inp[i,]$cellranger_dir;
+	id=inp[i,]$id;
+	if( file.exists(f) ){	
+		d=Read10X(data.dir=f)
+		d.list[[j]]= CreateSeuratObject(counts=d,project=id,min.cells=3,min.features=200)	
+		j = j + 1
+	}else{
+		warning(paste0(f," doesnot exist!"));
+	}
+}
+d = merge(d.list[[1]],d.list[2:length(d.list)])
+saveRDS(d,file=paste0(output,".rds"))
+EOF
+R --no-sve -f $2.rcmd
+
+
