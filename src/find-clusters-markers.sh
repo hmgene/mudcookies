4b66bc43a04ee3d73d527e5f8221bc7f83105b0b
Hyunmin Kim <human.gim@gmail.com>
2020-12-21 10:09:17 -0500

    Update find-clusters-markers.sh

--- src/find-clusters-markers.sh
+++ src/find-clusters-markers.sh
@@ -1,18 +1,30 @@
+#!/bin/bash
+usage="
+$BASH_SOURCE <seurat> <output_prefix> [resolution (default=0.5)]
+ <output_prefix>: use the same name as the <seurat> if you want to overwrite 
+"
+if [ $# -lt 1 ];then echo "$usage"; exit; fi
+res=${3:-0.5}
+
+cat<<'EOF'| sed s#INPUT.RES#$res# | sed s#OUTPUT#$2# | sed s#INPUT#$1#  #> $2.rcmd
+
+input="INPUT"
+input.res=INPUT.RES
+output="OUTPUT"
+
 require(dplyr)
 require(Seurat)
 
-input="../harmony/bladder-harmony-1.rds"
-output="bladder-harmony-1"
-input.res=0.2
-
 d=readRDS(input)
 d=FindClusters(d,graph.name="RNA_snn", resolution=input.res)
+saveRDS(d,paste0(output,".rds"))
+
 d.m = FindAllMarkers(d, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 write.csv(d.m,paste0(output,"_diffexp_","res",input.res,".csv"))
 
 top10 =  d.m %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
 d1 = d[, sample(colnames(d), size =2000, replace=F)]
 pdf(file=paste0(output,"_diffexp_","res",input.res,"top10.pdf"))
-DoHeatmap(d1,features=top10$gene) + NoLegend() +
-        theme(axis.text.y = element_text(size = 5))
+DoHeatmap(d1,features=top10$gene) + NoLegend()
 dev.off()
+EOF
