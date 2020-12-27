ecbf81104253bc036f0818b490b792b275418968
hyunmin kim <oannes@Hmhomes-Mac-Pro.local>
2020-12-27 11:58:58 -0500

    scpred

---
+++ src/train-celltype-with-scpred.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+usage="
+$BASH_SOURCE <seurat.obj> <scpred.obj>
+<seurat.obj>: cell_type annotation must be in meta.data
+<scpred.obj>: defaunt( out )
+"; if [ $# -lt 1 ];then echo "$usage";exit; fi
+
+ip=$1
+op=${2:-"out"}
+
+cat << 'EOF' | sed "s#INPUT#$ip#" | sed "s#OUTPUT#$op#" >$op.rcmd
+# for details read https://powellgenomicslab.github.io/scPred/articles/introduction.html 
+library("scPred")
+library("Seurat")
+library("magrittr")
+
+input = "INPUT"
+output= "OUTPUT"
+
+# test set
+if( !file.exists( input ) && input == "test" ){
+	d = scPred::pbmc_1
+}else{
+	d = readRDS(input);
+}
+
+# this is why we stick to the baseline norm 
+d = d %>%
+  NormalizeData() %>%
+  FindVariableFeatures() %>%
+  ScaleData() %>%
+  RunPCA()
+
+d = getFeatureSpace(d, "cell_type") # default svmRadial
+d = trainModel(d)
+# retraining : d<- trainModel(d, model = "mda", reclassify = c("cMono", "ncMono"))
+
+scpred=get_scpred(d)
+saveRDS(scpred,paste0(output,"_scpred.rds"))
+# new_embedding_aligned vs new_embedding : after and before harmony
+
+EOF
+
+R --no-save -f $op.rcmd 
