fb86e7b3fe2856055355c5ef0983eca27b7f54d9
Hyunmin Kim <human.gim@gmail.com>
2021-01-12 18:30:21 -0500

    Update rsem-install.sh

--- src/rsem-install.sh
+++ src/rsem-install.sh
@@ -1,14 +1,27 @@
 ## download gencode 
-download-rsem-files(){
-wget https://github.com/deweylab/RSEM/archive/v1.3.3.tar.gz
-tar -vzxf v1.3.3.tar.gz
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/GRCh38.primary_assembly.genome.fa.gz
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/GRCh38.primary_assembly.genome.fa
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/gencode.v24.annotation.gtf.gz
-wget http://genomedata.org/rnaseq-tutorial/HBR_UHR_ERCC_ds_5pc.tar
-gunzip *.gz
+download-gencode-files(){
+        mkdir -p gencode
+        cd gencode
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M25/GRCm38.primary_assembly.genome.fa.gz
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M25/gencode.vM25.annotation.gtf.gz
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/GRCh38.primary_assembly.genome.fa.gz
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/gencode.v36.annotation.gtf.gz
+        gunzip *.gz
+        cd ..
 }
-
+install-rsem(){
+        wget https://github.com/deweylab/RSEM/archive/v1.3.3.tar.gz
+        tar -vzxf v1.3.3.tar.gz
+        cd RSEM-1.3.3
+        make
+        ..
+}
+download-rsem-example-files(){
+        mkdir -p data
+        cd data
+        wget http://genomedata.org/rnaseq-tutorial/HBR_UHR_ERCC_ds_5pc.tar
+        cd ..
+}
 echo "#!/bin/bash
 #SBATCH --job-name=rsem-ref
 #SBATCH --time=4-23:00:00
