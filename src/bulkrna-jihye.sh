043948f1bd2e8e4da62a3d7b6fdc6e61820521dd
ppocppoc2 <76707451+ppocppoc2@users.noreply.github.com>
2021-01-13 13:16:11 -0500

    Update bulkrna-jihye.sh

--- src/bulkrna-jihye.sh
+++ src/bulkrna-jihye.sh
@@ -1,1 +1,2 @@
+# illumina adaptor
 cuffdiff -p 8 -o /beevol/home/kimji/Ghosh/Ghosh_Lee/ultra109/ultra109_131_individual_FPKM -b  /beevol/home/kimji/Bowtie_index/GRCh38/UCSC/Sequence/Bowtie2Index/genome.fa -u /beevol/home/kimji/Bowtie_index/GRCh38/UCSC/Annotation/Genes/ChrRemoved_genes.gtf 1-1rtbulk_sorted.bam 2-rtpo_sorted.bam 3-1nv_sorted.bam 4-1nvdf_sorted.bam 5-1rt_sorted.bam 6-1rtdf_sorted.bam
