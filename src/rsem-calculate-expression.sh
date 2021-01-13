8ac488aad096269454781aba990d244e5e2016e5
Hyunmin Kim <human.gim@gmail.com>
2021-01-13 09:56:35 -0500

    Update rsem-calculate-expression.sh

--- src/rsem-calculate-expression.sh
+++ src/rsem-calculate-expression.sh
@@ -2,15 +2,18 @@
 
 usage="$BASH_SOURCE <fastq1> <fastq2> <ref> <output> [-p <nproc>]"
 if [ $# -lt 4 ];then echo "$usage"; exit; fi
+
 star=`which STAR` #/cm/shared/apps/STAR/2.7.6a/STAR
 star=${star%/*}
 odir=$4
+isgzip=""
+[[ $1 =~ .*gz ]] && isgzip="--star-gzipped-read-file"
 
 mkdir -p ${odir%/*} # RSEM error
-rsem-calculate-expression -p $nproc --paired-end \
+rsem-calculate-expression -p $nproc --paired-end $isgzip \
         --star --star-path $star \
-        --star-gzipped-read-file \
         --estimate-rspd \
         --append-names \
         --no-bam-output \
         $@
+~                                              
