7a3b20886756956f70052310d06b4c597fa2947c
Hyunmin Kim <human.gim@gmail.com>
2021-01-22 15:21:36 -0500

    Update cluster-seurat.sh

--- src/cluster-seurat.sh
+++ src/cluster-seurat.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 usage="
-$BASH_SOURCE <input_seurat.obj> <output_seurat.obj> [resolution ] [nproc]
- [resolution] : default 0.5
+$BASH_SOURCE <input_seurat.obj> <output_seurat.obj> [options]
+[options]:
+ -res=<float> : default 0.5
+ -nproc=<int> : default 1
 "
 if [ $# -lt 1 ];then echo "$usage"; exit; fi
-res=${3:-0.5}
-nproc=${4:-1}
+res=`echo ${@:2} | perl -ne 'print $_=~/res=([\d\.]+)/ ? $1 : 0.5'`
+nproc=`echo ${@:2} | perl -ne 'print $_=~/nproc=(\d+)/ ? $1 : 1'`
 
 cat<<'EOF'| sed s#INPUT.RES#$res# | sed s#OUTPUT#$2# | sed s#INPUT#$1#\
 | sed "s#NPROC#$nproc#"  > $2.rcmd
@@ -18,8 +20,8 @@ require(Seurat)
 d=readRDS(input)
 
 if( nproc > 1 ){
-	require("future");
-	plan("multiprocess",workers=nproc);
+        require("future");
+        plan("multiprocess",workers=nproc);
 }
 
 
@@ -27,5 +29,4 @@ d=FindNeighbors(d, reduction="harmony",dims=1:50)
 d=FindClusters(d,graph.name="RNA_snn", resolution=input.res)
 saveRDS(d,output)
 EOF
-cat $2.rcmd
-#R --no-save -f $2.rcmd
+R --no-save -f $2.rcmd
