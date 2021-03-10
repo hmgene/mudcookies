c2ce5bde5633ebdbcb996b3888b3319f9b73a513
Hyunmin Kim <human.gim@gmail.com>
2021-03-10 11:43:00 -0500

    Update seurat-findmarkers-example.r

--- src/seurat-findmarkers-example.r
+++ src/seurat-findmarkers-example.r
@@ -1,10 +1,16 @@
 ## select target resolution
 input="seurat-object.rds"
 output="out.csv"
+nproc=1
 
 d=readRDS(input)
 ## select a target cluster result
 Idents(object=d) = d@meta.data$RNA_snn_res.0.2
-## need parallization
+
+## modify nproc > 1 if parallization needed
+if( nproc > 1 ){
+        require("future");
+        plan("multiprocess",workers=nproc);
+}
 x=FindAllMarkers(d)
 write.csv(x,file=output)
