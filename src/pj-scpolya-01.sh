06f36e73d21dae117dee69cf0ebe1d5d1f7cb4e8
Hyunmin Kim <human.gim@gmail.com>
2021-03-15 09:12:33 -0400

    Create pj-scpolya-01.sh

---
+++ src/pj-scpolya-01.sh
@@ -0,0 +1,78 @@
+
+sam_to_bed12(){
+usage="$FUNCNAME <sam> [-x]
+"
+# reference from https://samtools.github.io/hts-specs/SAMv1.pdf
+if [ $# -lt 1 ]; then echo "$usage"; return; fi
+        cat $1 | perl -ne 'chomp; my @a=split/\t/,$_; my $print_seq="'${2:-}'";
+                if($_=~/^@/){ next;}
+                my $id=$a[0];
+                my $flag=$a[1];
+                my $chrom=$a[2];
+                next if $chrom eq "*";
+                my $start=$a[3]-1;
+                my $mapq=$a[4]; # -10log10 Pr( wrong )
+                my $cigar=$a[5];
+                my $seq=$a[9];
+                my $len=0;
+                my $strand="+"; if ( $flag & (0x10) ){ $strand="-"; }
+                my $gseq=""; # genomic sequence 
+                #\*|([0-9]+[MIDNSHPX=])+ 
+                my $cb=""; #single-cell barcode
+                if($_=~/CB:Z:([\w\d-]+)/){ $cb=$1;};
+
+                my @starts=(); 
+                my @sizes=(); 
+                my @seqs=();
+                my $gpos=0; 
+                my $spos=0; ## sequence offset
+                my $prev_c="";
+                while($cigar=~/(\d+)([MIDNSHPX=])/g){ 
+                        my ($x,$c)=($1,$2);
+                        if($c=~/[MX=]/){
+                                if($prev_c eq "D" or $prev_c eq "I"){
+                                        $sizes[$#sizes] += $x;
+                                        $seqs[$#seqs] .= substr($seq,$spos,$x);
+                                }else{
+                                        push @starts, $gpos; 
+                                        push @sizes, $x;
+                                        push @seqs, substr($seq,$spos,$x);
+                                }
+                                $spos += $x;
+                                $gpos += $x;
+                        }elsif($c=~/[DN]/){
+                                if( $c eq "D" ){
+                                        $seqs[$#seqs] .= "-"x$x;
+                                        $sizes[$#sizes] += $x; ## ignore deletions
+                                }else{ ## splicing?
+                                }
+                                $gpos += $x; 
+                       }elsif($c=~/[SI]/){
+                                ## note that soft/hard clipping does not affect genomic coordinates 
+                                $spos += $x;
+                        }else{
+                                ## P is not handled
+                        }
+                        $prev_c=$c;
+                }
+                my $end=$start+$starts[$#starts]+$sizes[$#sizes];
+                my $str_sizes=join(",",@sizes);
+                my $str_starts=join(",",@starts);
+                my $str_seqs=join(",",@seqs);
+                if($print_seq eq "-x"){
+                        $str_starts .= "\t".$str_seqs;
+                }
+                print join("\t",( 
+                        $chrom,$start,$end,$cb,$mapq,$strand,
+                        $start,$end,"0,0,0",scalar @starts,
+                        $str_sizes,$str_starts
+                )),"\n";
+        '
+}
+
+for bam in bam/*.bam;do
+        n=${bam#*\.}
+        n=${n%\.bam}
+        samtools view -b $bam | sam_to_bed12 -  | head
+        exit
+done
