21a7a93bdda1045c291a5b1bce79c93f7357cb17
Hyunmin Kim <human.gim@gmail.com>
2021-07-20 11:18:11 +0900

    Update seurat-merge-cluster-marker.sh

--- src/seurat-merge-cluster-marker.sh
+++ src/seurat-merge-cluster-marker.sh
@@ -1,36 +1,19 @@
-## input parameters
-idir="data"
-out="data/3n3p" ## big files > 100Mb
+## input parameters (project_name. matrix_directory)
+input="
+LB6 /w/LB6/outs/filtered_feature_bc_matrix/
+RLL /w/RLL/outs/filtered_feature_bc_matrix/
+"
+output="LB6_RLL"
 nproc=6
 
-# ref: https://satijalab.org/seurat/articles/integration_large_datasets.html
-library(data.table)
-library(stringr)
+##
 library(Seurat)
 library(parallel)
-library("R.utils")
-
-
-# ref: https://satijalab.org/seurat/articles/integration_introduction.html
-l=mclapply(X =  list.files(idir,pattern=".txt.gz"), mc.cores=nproc, FUN = function(x) {
-        y=fread(paste0(idir,"/",x)); z=y[,-1]; row.names(z)=y$GENE;
-        z=CreateSeuratObject(count=z, project=str_extract(x,"GSM\\d+"));
-        #z=NormalizeData(z,verbose=F)
-        #z=FindVariableFeatures(z, verbose=F)
-        return(z)
-})
-
-
-
-
-nproc=6
-# ref: https://satijalab.org/seurat/articles/integration_introduction.html
-l=mclapply(X =  list.files(idir,pattern=".txt.gz"), mc.cores=nproc, FUN = function(x) {
-        y=fread(paste0(idir,"/",x)); z=y[,-1]; row.names(z)=y$GENE;
-        z=CreateSeuratObject(count=z, project=str_extract(x,"GSM\\d+"));
-        #z=NormalizeData(z,verbose=F)
-        #z=FindVariableFeatures(z, verbose=F)
-        return(z)
+inp=read.table(text=input)
+l=mclapply(X=1:nrow(inp), mc.cores=nproc,FUN=function(i) {
+        expression_matrix <- Read10X(data.dir = inp[i,]$V2)
+        seurat_object = CreateSeuratObject(counts = expression_matrix, project=inp[i,]$V1)
+        seurat_object
 })
 
 d=merge(l[[1]],l[2:length(l)])
@@ -44,18 +27,17 @@ print(d[["pca"]], dims = 1:5, nfeatures = 5)
 VizDimLoadings(d, dims = 1:2, reduction = "pca")
 d <- JackStraw(d, num.replicate = 100)
 d <- ScoreJackStraw(d, dims = 1:20)
-JackStrawPlot(d, dims = 1:15)
+#JackStrawPlot(d, dims = 1:15)
 
 d <- FindNeighbors(d, dims = 1:20)
 d <- FindClusters(d, resolution = 0.5)
 d <- RunUMAP(d, dims = 1:20)
-saveRDS(d,paste0(out,".rds"))
-
+saveRDS(d,paste0(output,".rds"))
 
 ## markers
 tmp=mclapply(X = names(table(d$seurat_clusters)), mc.cores=nproc,FUN = function(x) {
         m=FindMarkers(d,ident.1=x, only.pos=T)
-        write.csv(m[order(-m$avg_log2FC),],file=paste0(out,"_marker_",x,".csv"))
+        write.csv(m[order(-m$avg_log2FC),],file=paste0(output,"_marker_",x,".csv"))
 })
 
 
