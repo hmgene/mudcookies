a13bd24a548aa7785ed154f1a73e2e31611d5b3e
Hyunmin Kim <human.gim@gmail.com>
2022-12-21 14:38:47 -0500

    Update sam.sh

--- src/sam.sh
+++ src/sam.sh
@@ -1,11 +1,12 @@
 bam2fq(){
 for f in *.bam;do
         n=${f%.RNA-Seq.bam}
-        cat<<-eof  | sbatch
+        if [ -f ${n}_R1.fastq.gz ];then continue; fi
+        cat<<-eof  #| sbatch
         #!/bin/bash 
-        odule load samtools
+        module load samtools
         samtools view -F0x100 -hf0x2 $f | samtools fastq - -n -1 ${n}_R1.fastq.gz -2 ${n}_R2.fastq.gz
         eof
-
 done
+
 }
