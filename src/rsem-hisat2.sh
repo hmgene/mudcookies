2272617a4b76420965654f6eab07dba00f193422
Hyunmin Kim <human.gim@gmail.com>
2023-01-05 15:25:33 -0500

    Update rsem-hisat2.sh

--- src/rsem-hisat2.sh
+++ src/rsem-hisat2.sh
@@ -1,2 +1,34 @@
 # https://www.biostars.org/p/347818/
-# todo
+
+
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/GRCh38.primary_assembly.genome.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/gencode.v42.annotation.gtf.gz
+gunzip -dc *.gz;
+
+gtf=gencode.v42.annotation.gtf
+fa=GRCh38.primary_assembly.genome.fa
+## change this to where hisat2 exists
+hp=/usr/local/hisat2/2.2.0/bin
+ref="rsem_hisat2_hg38"
+
+## input fastq files
+fq1=x_R1.fastq.gz
+fq2=x_R2.fastq.gz
+
+
+## index
+echo "#!/bin/bash
+module load gcc
+module load hisat2
+rsem-prepare-reference -p 8 --gtf $gtf  --hisat2-hca --hisat2-path $hp $fa $ref
+" #| sbatch  --mem=200g -c 8
+
+## mapping and RSEM
+echo "#!/bin/bash
+hisat2 -x $ref  -1 $fq1 -2 $fq2 -p 8 \
+    --no-spliced-alignment  --end-to-end  --no-softclip  --rdg 10000,10000  --rfg 10000,10000 > $o.sam
+
+samtools view -hb $o.sam | samtools view -bh -f 0x2 -F 0x100 - | samtools sort -n > $o.bam
+
+rsem-calculate-expression --paired-end --bam $o.bam rsem $o
+" #| sbatch --mem=16g -c 8
