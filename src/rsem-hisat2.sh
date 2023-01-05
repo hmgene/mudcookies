7763a133277e1c59c0b8a5b57d96d123ad920163
Hyunmin Kim <human.gim@gmail.com>
2023-01-05 15:30:11 -0500

    Update rsem-hisat2.sh

--- src/rsem-hisat2.sh
+++ src/rsem-hisat2.sh
@@ -1,9 +1,7 @@
-# https://www.biostars.org/p/347818/
 
-
-wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/GRCh38.primary_assembly.genome.fa.gz
-wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/gencode.v42.annotation.gtf.gz
-gunzip -dc *.gz;
+#wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/GRCh38.primary_assembly.genome.fa.gz
+#wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_42/gencode.v42.annotation.gtf.gz
+#gunzip -dc *.gz;
 
 gtf=gencode.v42.annotation.gtf
 fa=GRCh38.primary_assembly.genome.fa
@@ -17,20 +15,19 @@ fq2=x_R2.fastq.gz
 
 
 ## index
-cat<<-eof | sbatch  --mem=200g -c 8 
+cat<<-eof | sbatch  --mem=200g -c 8
 #!/bin/bash
 module load gcc
 module load hisat2
 rsem-prepare-reference -p 8 --gtf $gtf  --hisat2-hca --hisat2-path $hp $fa $ref
-eof 
+eof
 
 ## mapping and RSEM
-cat<<-eof 
-#!/bin/bash | sbatch --mem=16g -c 8
+cat<<-eof | sbatch --mem=16g -c 8
+#!/bin/bash
 hisat2 -x $ref  -1 $fq1 -2 $fq2 -p 8 \
     --no-spliced-alignment  --end-to-end  --no-softclip  --rdg 10000,10000  --rfg 10000,10000 > $o.sam
-
 samtools view -hb $o.sam | samtools view -bh -f 0x2 -F 0x100 - | samtools sort -n > $o.bam
-
 rsem-calculate-expression --paired-end --bam $o.bam rsem $o
 eof
+
