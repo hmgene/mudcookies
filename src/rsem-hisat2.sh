31d83c4d1932d35566bdd69773a0afc8eedbf349
Hyunmin Kim <human.gim@gmail.com>
2023-01-05 15:28:09 -0500

    Update rsem-hisat2.sh

--- src/rsem-hisat2.sh
+++ src/rsem-hisat2.sh
@@ -17,18 +17,20 @@ fq2=x_R2.fastq.gz
 
 
 ## index
-echo "#!/bin/bash
+cat<<-eof | sbatch  --mem=200g -c 8 
+#!/bin/bash
 module load gcc
 module load hisat2
 rsem-prepare-reference -p 8 --gtf $gtf  --hisat2-hca --hisat2-path $hp $fa $ref
-" #| sbatch  --mem=200g -c 8
+eof 
 
 ## mapping and RSEM
-echo "#!/bin/bash
+cat<<-eof 
+#!/bin/bash | sbatch --mem=16g -c 8
 hisat2 -x $ref  -1 $fq1 -2 $fq2 -p 8 \
     --no-spliced-alignment  --end-to-end  --no-softclip  --rdg 10000,10000  --rfg 10000,10000 > $o.sam
 
 samtools view -hb $o.sam | samtools view -bh -f 0x2 -F 0x100 - | samtools sort -n > $o.bam
 
 rsem-calculate-expression --paired-end --bam $o.bam rsem $o
-" #| sbatch --mem=16g -c 8
+eof
