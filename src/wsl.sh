e7aa54e15b78fdc678db27e60e204b4ff7a2098a
Hyunmin Kim <human.gim@gmail.com>
2024-07-12 16:01:19 -0400

    Update wsl.sh

--- src/wsl.sh
+++ src/wsl.sh
@@ -5,4 +5,7 @@
   netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=172.30.53.115 connectport=22
   netsh advfirewall firewall add rule name="Open Port 22 for WSL2" dir=in action=allow protocol=TCP localport=22
   netsh interface portproxy show v4tov4
+
+
+129.22.242.198
 ```
